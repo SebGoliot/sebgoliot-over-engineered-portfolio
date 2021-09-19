6af62b2c539bb10aaf71383a5eaf4d4a988b500a
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-09-19 21:05:33 +0200

    Added sql tables for achievements and interests

--- sgoep-api/sql/create-tables.sql
+++ sgoep-api/sql/create-tables.sql
@@ -14,4 +14,23 @@ CREATE TABLE `author` (
   `about` TEXT(512),
   `github` VARCHAR(64),
   `linkedin` VARCHAR(64)
-)
+);
+
+CREATE TABLE `achievements` (
+	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+	`name` VARCHAR(64),
+	`subtitle` VARCHAR(128),
+	`desc` TEXT(512),
+	`icon` VARCHAR(64),
+	`link` VARCHAR(128),
+	`link_icon` VARCHAR(64),
+	`github` VARCHAR(128),
+  `tech` TEXT(512)
+);
+
+CREATE TABLE `interests` (
+	`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+	`name` VARCHAR(64),
+	`icon` VARCHAR(64),
+	`desc` TEXT(512)
+);
