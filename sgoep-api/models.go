f17133026a5f557185b0381aa912d2d92c2d8688
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-09-19 21:02:11 +0200

    Added achievements and interests models

--- sgoep-api/models.go
+++ sgoep-api/models.go
@@ -17,3 +17,22 @@ type Author struct {
 	Github   string `json:"github" db:"github"`
 	Linkedin string `json:"linkedin" db:"linkedin"`
 }
+
+type Achievement struct {
+	Id        string   `json:"id" db:"id"`
+	Name      string   `json:"name" db:"name"`
+	Subtitle  string   `json:"subtitle" db:"subtitle"`
+	Desc      string   `json:"desc" db:"desc"`
+	Icon      string   `json:"icon" db:"icon"`
+	Link      string   `json:"link" db:"link"`
+	Link_icon string   `json:"link_icon" db:"link_icon"`
+	Github    string   `json:"github" db:"github"`
+	Tech      []string `json:"tech" db:"tech"`
+}
+
+type Interest struct {
+	Id   string `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
+	Icon string `json:"icon" db:"icon"`
+	Desc string `json:"desc" db:"desc"`
+}
