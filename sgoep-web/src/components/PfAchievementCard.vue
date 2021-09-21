af42eacb878dfac992cf11887ad63896a0359064
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-09-21 13:05:28 +0200

    WIP - Added Achievements and AchievementCard

---
+++ sgoep-web/src/components/PfAchievementCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="card-list section-margin">
+    <div
+      class="thing-card card"
+      v-for="(ach, key) in achievements.slice().reverse()"
+      :key="key"
+    >
+      <header>
+        <div>
+          <span class="material-icons-round">{{ ach.icon }}</span>
+          <h3 class="card-title">&nbsp;{{ ach.name }}</h3>
+          <div class="availability">
+            <img
+              :src="require('@/assets/svg/' + ach.link_icon + '-icon.svg')"
+              :alt="ach.link_icon"
+            />
+          </div>
+        </div>
+        <h4>{{ ach.subtitle }}</h4>
+      </header>
+      <p>
+        <span v-for="(line, key) in ach.desc.split('\\n')" :key="key">
+          {{ line }} <br />
+        </span>
+      </p>
+      <div class="tech">
+        <img
+          v-for="(t, key) in ach.tech"
+          :key="key"
+          :src="require('@/assets/svg/' + t + '-icon.svg')"
+          :alt="t"
+        />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "PfAchievementCard",
+  props: {
+    achievements: {},
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ sgoep-web/src/components/PfAchievements.vue
@@ -0,0 +1,28 @@
+<template>
+  <img class="wave-to-blue" src="../assets/svg/wave-to-blue.svg" alt="" />
+  <div class="wrapper-blue">
+    <section>
+      <header>
+        <span class="material-icons-round">integration_instructions</span>
+        <h2>&nbsp;Réalisations</h2>
+      </header>
+      <PfAchievementCard :achievements="achievements" />
+    </section>
+  </div>
+  <img src="../assets/svg/wave-to-alpha.svg" alt="" />
+</template>
+
+<script>
+import PfAchievementCard from "./PfAchievementCard";
+
+export default {
+  name: "PfAchievements",
+  props: {
+    achievements: {},
+  },
+  components: {
+    PfAchievementCard,
+  },
+};
+
+</script>
